5ca37f42516d075228e9fda784663163aa5a2b91
Keith Jung <60eokk@gmail.com>
2024-07-18 17:08:41 -0500

    setting up stage for huggingface tokenization

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-def main():
-    return 0
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login
+
+# TOKEN = 
+login(TOKEN)
+
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b")
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
+
+
+
+# def main():
+#     return 0
+
